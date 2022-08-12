da9a7c5b664f42d14cfb93273ab24dd56a84b5f5
rafaeIferreira <raffaferr@gmail.com>
2022-08-12 15:17:26 -0300

    update commit

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,7 +8,7 @@ import {
   import Default from '../Routes/Default/Default'
   import SummonerSearch from '../Routes/SummonerSearch/SummonerSearch';
   import Rank from '../Routes/Rank/Rank';
-  const API_KEY = "RGAPI-a84d00c7-2815-4576-8eee-ab370386ecb0";
+  const API_KEY = "RGAPI-d2f22471-3ac6-401f-b3ad-3a08d48369c5";
 const Main = ()=>{
     return(
         <div>
