2dabcb2c1c518ab9f141324326398821951cbcd1
Rafael Ferreira <35198705+rafaeIferreira@users.noreply.github.com>
2023-03-14 20:43:21 -0300

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,7 +8,8 @@ import {
   import Default from '../Routes/Default/Default'
   import SummonerSearch from '../Routes/SummonerSearch/SummonerSearch';
   import Rank from '../Routes/Rank/Rank';
-  const API_KEY = "RGAPI-d2f22471-3ac6-401f-b3ad-3a08d48369c5";
+  //Sua API KEY AQUI
+  const API_KEY = "";
 const Main = ()=>{
     return(
         <div>
